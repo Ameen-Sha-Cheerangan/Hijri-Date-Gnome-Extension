bb91b5a0b27ff671ff9eb7932038b15c2451d9f3
Ameen-Sha-Cheerangan <101210452+Ameen-Sha-Cheerangan@users.noreply.github.com>
2023-05-21 17:26:34 +0530

    Update extension.js

--- my-hijri-date-extension@ameen-sha/extension.js
+++ my-hijri-date-extension@ameen-sha/extension.js
@@ -4,26 +4,27 @@ const Main = imports.ui.main;
 let dateLabel;
 
 function setDate() {
-  const hijriDate = new Intl.DateTimeFormat('en-US-u-ca-islamic', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
-  }).format(new Date());
+    const hijriDate = new Intl.DateTimeFormat("en-US-u-ca-islamic", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    }).format(new Date());
 
-  dateLabel.set_text(`Hijri Date: ${hijriDate}`);
+    dateLabel.set_text(`${hijriDate}`);
 }
 
 function init() {
-  dateLabel = new St.Label();
-  setDate();
+    dateLabel = new St.Label({
+        style: "text-align: center; position: relative; top: 50%; transform: translateY(-50%);",
+    });
+    setDate();
 }
 
 function enable() {
-  Main.panel._rightBox.insert_child_at_index(dateLabel, 1);
-  Main.panel._rightBox.set_child_at_index(dateLabel, 1);
+    Main.panel._leftBox.insert_child_at_index(dateLabel, 1);
+    Main.panel._leftBox.set_child_at_index(dateLabel, 1);
 }
 
 function disable() {
-  Main.panel._rightBox.remove_child(dateLabel);
+    Main.panel._leftBox.remove_child(dateLabel);
 }
-
