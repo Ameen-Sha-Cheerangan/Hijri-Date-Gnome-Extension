1e10845f79bf6b04d0038035f4a928b4dd374353
Ameen-Sha-Cheerangan <101210452+Ameen-Sha-Cheerangan@users.noreply.github.com>
2023-05-21 17:42:53 +0530

    Update extension.js

--- my-hijri-date-extension@ameen-sha/extension.js
+++ my-hijri-date-extension@ameen-sha/extension.js
@@ -4,7 +4,7 @@ const Main = imports.ui.main;
 let dateLabel;
 
 function setDate() {
-    const hijriDate = new Intl.DateTimeFormat("en-US-u-ca-islamic", {
+    const hijriDate = new Intl.DateTimeFormat("ar-IN-u-ca-islamic", {
         day: "numeric",
         month: "long",
         year: "numeric",
@@ -15,7 +15,7 @@ function setDate() {
 
 function init() {
     dateLabel = new St.Label({
-        style: "text-align: center; position: relative; top: 50%; transform: translateY(-50%);",
+        style: "text-align: center; position: relative; top: 50%; transform: translateY(-50%); padding-left: 10px; padding-right: 5px; padding-bottom: 3px; padding-top: 3px;",
     });
     setDate();
 }
