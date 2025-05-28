3aaa9263984d422940c0fb04606acc302bbbad1b
Ameen-Sha-Cheerangan <101210452+Ameen-Sha-Cheerangan@users.noreply.github.com>
2025-05-28 11:14:10 +0530

    Update extension.js

--- my-hijri-date-extension@ameen-sha/extension.js
+++ my-hijri-date-extension@ameen-sha/extension.js
@@ -1,10 +1,14 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
+const ExtensionUtils = imports.misc.extensionUtils;
 
 let dateLabel;
+let settings;
+let settingsChangedSignal;
+let colorChangedSignal;
 
 function setDate() {
-    const change = -1; // Change this value to 0, -1, or 1 to adjust the date
+    const change = settings.get_int('date-adjustment');
     const currentDate = new Date();
     currentDate.setDate(currentDate.getDate() + change);
 
@@ -14,23 +18,39 @@ function setDate() {
         year: "numeric",
     });
     const modifiedHijriDate = formatter.format(currentDate);
+    // Get color from settings (default to white if not set)
+    let color = settings.get_string('mycolor') || 'rgba(255,255,255,1)';
+    dateLabel.set_style(`color: ${color};`);
     dateLabel.set_text(`${modifiedHijriDate}`);
 }
 
 function init() {
-
+    // Initialize GSettings
+    settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.my-hijri-date-extension');
 }
 
 function enable() {
     dateLabel = new St.Label({
-        style: "text-align: center; position: relative; top: 50%; transform: translateY(-50%);  padding-left: 20px; padding-right: 15px; padding-bottom: 3px; padding-top: 3px;",
+        style_class: "hijri-date-label",
     });
     setDate();
     Main.panel._leftBox.insert_child_at_index(dateLabel, 1);
     Main.panel._leftBox.set_child_at_index(dateLabel, 1);
+    // Listen for changes in the setting and update date
+    settingsChangedSignal = settings.connect('changed::date-adjustment', setDate);
+    colorChangedSignal = settings.connect('changed::mycolor', setDate);
+
 }
 
 function disable() {
+    if (settingsChangedSignal) {
+        settings.disconnect(settingsChangedSignal);
+        settingsChangedSignal = null;
+    }
+    if (colorChangedSignal) {
+        settings.disconnect(colorChangedSignal);
+        colorChangedSignal = null;
+    }
     Main.panel._leftBox.remove_child(dateLabel);
     dateLabel.destroy();
     dateLabel = null;
